67430d20db8391ba20bf8f33a7962edaf66f656a
Mohd Shadab <shadab_ibrahim@outlook.com>
2023-06-27 21:18:01 +0530

    submitting code-Tue Jun 27 2023 21:18:01 GMT+0530 (India Standard Time)

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
 
+let userInput = document.querySelector("#newTodoInput");
+
+let submit = document.querySelector("#addTodoBtn");
+let ol = document.querySelector("#todoList");
+
+submit.addEventListener('click', function (e) {
+    // e.preventDefault();
+    let li = document.createElement('li');
+    li.innerHTML = userInput.value;
+    if (userInput.value === "") {
+        alert("Please enter a todo item")
+    } else {
+        ol.appendChild(li);
+    }
+
+})
